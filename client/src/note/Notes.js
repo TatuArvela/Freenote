b238e2b56ab25c664cb019e8522b4b0fac4ac7d9
arvelat <tatu.arvela@cgi.com>
2017-09-14 12:45:17 +0300

    Style improvements

--- client/src/note/Notes.js
+++ client/src/note/Notes.js
@@ -3,7 +3,6 @@ import { connect } from 'preact-redux';
 
 import reduce from '../app/reducers';
 import * as noteActions from '../note/noteActions';
-import AddNote from './AddNote';
 import Note from './Note';
 
 @connect(reduce, {...noteActions})
@@ -16,7 +15,6 @@ export default class Notes extends Component {
         return (
             <div class="notes">
                 <div class="notes-grid">
-                    <AddNote />
                     {[...notes].reverse().map(note => (
                         <Note key={note.id} note={note} onRemove={this.removeNote} />
                     ))}
