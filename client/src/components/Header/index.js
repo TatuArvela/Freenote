d22ca06637c07e10bdd83b193fc42b3f5f3acc3b
Tatu Arvela <tatu.arvela@gmail.com>
2017-12-01 00:21:16 +0200

    Responsive layout improvements

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -21,26 +21,27 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 
 let Header = ({ toolbar, switchTab }) => (
   <div className="header">
-    <div className="header-tabs">
-        <h1 className="window-title">Freenote</h1>
+    <h1 className="window-title">Freenote</h1>
 
-        <a
-          className={`header-tab ${toolbar === 'EDIT' ? 'active' : ''}`}
-          onClick={() => switchTab('EDIT')}
-        >Edit</a>
-
-        <a
-          className={`header-tab ${toolbar === 'VIEW' ? 'active' : ''}`}
-          onClick={() => switchTab('VIEW')}
-        >View</a>
-
-        <div className="search">
-            <i className="material-icons">search</i>
-            <input type="text" placeholder="Search..."/>
-        </div>
-        
-        <div className="user-name">Tatu Arvela</div>
+    <div className="user-name">Tatu Arvela</div>
+    <div className="user-icon">
+      <i className="material-icons">person</i>
     </div>
+
+    <div className="search">
+        <i className="material-icons">search</i>
+        <input type="text" placeholder="Search..."/>
+    </div>
+
+    <a
+      className={`header-tab ${toolbar === 'EDIT' ? 'active' : ''}`}
+      onClick={() => switchTab('EDIT')}
+    >Edit</a>
+
+    <a
+      className={`header-tab ${toolbar === 'VIEW' ? 'active' : ''}`}
+      onClick={() => switchTab('VIEW')}
+    >View</a>
 
     {toolbar === "EDIT" &&
       <Edit />
